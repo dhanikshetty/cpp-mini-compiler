63d8920a70a2f0d96171c4c3a03bd5b7b87abf45
Dhanik Shetty <60191266+dhanikshetty@users.noreply.github.com>
2021-06-25 00:36:41 +0530

    Add files via upload

---
+++ Abstract_Syntax_Tree/input.cpp
@@ -0,0 +1,14 @@
+int main()
+{
+    int a = 4;
+    for(int i = 1; i < 10; i = i + 1)
+    {
+        int b = 6;
+        while()
+        {
+            int c = 7;
+            if()
+                long d = 4*8;
+        }
+    }
+}
